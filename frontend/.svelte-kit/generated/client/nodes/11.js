0183ea73bd5edbe504a5e2e0149a19eb0e72bf35
Jeanphilo Gong <488034418@qq.com>
2026-01-18 10:40:34 +0800

    Revert "chore(repo): remove tracked artifacts"
    This reverts commit bfb74ed1f6b60bac1953d0baba438a0066ca4239.

--- frontend/.svelte-kit/generated/client/nodes/11.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/configs/create/+page.svelte";
--- frontend/.svelte-kit/generated/client/nodes/19.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/index/upload/+page.svelte";
